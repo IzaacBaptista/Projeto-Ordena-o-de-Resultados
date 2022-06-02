5d06a999f6ad97ea1214f264e516755f0b3a9747
Izaac Baptista <izaacbaptista@gmail.com>
2022-06-02 07:49:26 -0300

    conectando ao banco

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
-    try {
-        $pdo = new PDO("mysql:dbname=projeto_ordenar;host=localhost", "root", "kodejifr")
+    try{
+    $pdo = new PDO("mysql:dbname=projeto_ordenar;host=localhost", "root", "kodejifr");
+    } catch(PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
     }
 
 
